75d4b25f49117b191cd061cdb060df5380e0e5c1
Diony S. Castro <dnycstr@gmail.com>
2021-12-04 12:11:44 +0800

    Basic AspNetIdentity

--- README.md
+++ README.md
@@ -5,12 +5,43 @@ This repository contains test security projects using IdentityServer4 and ASP.NE
 
 # Projects
 
-	1. Auth.Level01
+	**1. Auth.Level01**
 		This project contains basic ASP.NET MVC app with ASP.NET Identity
 
 		Reference:
-			[ASP.NET Identity](https://docs.microsoft.com/en-us/aspnet/core/security/authentication/identity)
-
+			[ASP.NET Identity](https://docs.microsoft.com/en-us/aspnet/core/security/authentication/identity)	
+		
 		Goal: 
 			a - Protect a page
 			b - Management of Users and Roles
+
+		Dependencies
+			```
+			    <PackageReference Include="Microsoft.AspNetCore.Identity" Version="2.2.0" />
+				<PackageReference Include="Microsoft.AspNetCore.Identity.EntityFrameworkCore" Version="5.0.12" />
+				<PackageReference Include="Microsoft.EntityFrameworkCore.SqlServer" Version="5.0.12" />
+				<PackageReference Include="Microsoft.EntityFrameworkCore.Tools" Version="5.0.12" />
+			```
+		
+		Add DbContext
+			```
+				public class ApplicationDbContext : IdentityDbContext
+				{
+					public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+					: base(options)
+					{
+					}
+				}
+			```
+
+		Configure Connection String
+			```
+				"ConnectionStrings": {
+					"DefaultConnection": "Server=DSCLAB-PC;Database=mitsauth-level01db;Trusted_Connection=True;MultipleActiveResultSets=true"
+				},
+			```
+
+		Initial Migration Command
+			`Add-Migration -Name "InitialDatabase" -Context ApplicationDbContext -OutputDir Data/Migrations`
+
+		`Update-Database`
--- src/Auth.Level01/Startup.cs
+++ src/Auth.Level01/Startup.cs
@@ -1,6 +1,9 @@
+using Auth.Level01.Data.Contexts;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -23,6 +26,48 @@ namespace Auth.Level01
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddIdentity<IdentityUser, IdentityRole>(options =>
+                    {
+                        options.SignIn.RequireConfirmedAccount = false;
+                    })
+                    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            services.Configure<IdentityOptions>(options =>
+            {
+                // Password settings.
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireNonAlphanumeric = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequiredLength = 6;
+                options.Password.RequiredUniqueChars = 1;
+
+                // Lockout settings.
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+                options.Lockout.MaxFailedAccessAttempts = 5;
+                options.Lockout.AllowedForNewUsers = true;
+
+                // User settings.
+                options.User.AllowedUserNameCharacters =
+                "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+                options.User.RequireUniqueEmail = false;
+            });
+
+            services.ConfigureApplicationCookie(options =>
+            {
+                // Cookie settings
+                options.Cookie.HttpOnly = true;
+                options.ExpireTimeSpan = TimeSpan.FromMinutes(5);
+
+                options.LoginPath = "/Users/Login";
+                options.AccessDeniedPath = "/Users/AccessDenied";
+                options.SlidingExpiration = true;
+            });
+
             services.AddControllersWithViews();
         }
 
