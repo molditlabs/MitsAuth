8b8c30edee769c07e81f78c9ec946e8ff6e39443
jrclarete <jhonrommel.clarete@gmail.com>
2021-12-05 14:59:47 +0800

    Created a registration/signup page

---
+++ src/Auth.Level01/Controllers/AccountController.cs
@@ -0,0 +1,29 @@
+﻿using Auth.Level01.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Auth.Level01.Controllers
+{
+    public class AccountController : Controller
+    {
+        [Route("signup")]
+        public IActionResult Signup()
+        {
+            return View();
+        }
+
+        [Route("signup")]
+        [HttpPost]
+        public IActionResult Signup(SignupUserModel userModel)
+        {
+            if (ModelState.IsValid)
+            {
+                ModelState.Clear();
+            }
+            return View();
+        }
+    }
+}
---
+++ src/Auth.Level01/Models/SignupUserModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Auth.Level01.Models
+{
+    public class SignupUserModel
+    {
+        [Required(ErrorMessage = "Please enter your Username")]
+        [Display(Name = "Username")]
+        public string Username { get; set; }
+        
+        [Required(ErrorMessage = "Please enter a strong Password")]
+        [Compare("ConfirmPassword", ErrorMessage = "Password does not match")]
+        [Display(Name = "Password")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Please confirm your Password")]
+        [Display(Name = "Confirm Password")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+    }
+}
